490defced6934860ee0ae0884c7d470bcb0c3a61
rishabh9112 <133814213+rishabh9112@users.noreply.github.com>
2023-12-29 20:15:36 +0000

    first change

---
+++ codeBasics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 123456
+let accountEmail="rishabhagrawal9112@gmail.com"
+var password="1234566"
+accountAdd="jaipur"
+ accountId = 123456
+accountEmail="rishabhagrawal912@gmail.com"
+ password="1234566"
+accountAdd="jaipur"
+console.table([accountId ,accountEmail, password, accountAdd])
