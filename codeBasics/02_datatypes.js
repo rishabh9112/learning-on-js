d32880361ec6d61a2a6a068c89b11f3184d592e3
rishabh9112 <133814213+rishabh9112@users.noreply.github.com>
2023-12-30 06:45:56 +0000

    datatypes

---
+++ codeBasics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)//we are using nodejs ,not browser
+
+console.log(3 + 3); // make sure your code readablity should be high
+
+// for documentation follow mdn and tc39.es
+
+let name = "rishabh";
+let age = 20;
+let isloggedIn = false;
+
+//number => 2 to the power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null  => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+console.log(typeof age); //number
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
