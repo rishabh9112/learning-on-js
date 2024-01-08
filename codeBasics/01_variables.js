a573d7c75d885c65dce554e7971f4d6264b54305
rishabh9112 <133814213+rishabh9112@users.noreply.github.com>
2024-01-08 13:06:57 +0000

    commit all

--- codeBasics/01_variables.js
+++ codeBasics/01_variables.js
@@ -7,3 +7,7 @@ accountEmail="rishabhagrawal912@gmail.com"
  password="1234566"
 accountAdd="jaipur"
 console.table([accountId ,accountEmail, password, accountAdd])
+
+/* prefer not to use var 
+because  of issue in block scope and fuctional scope
+*/
